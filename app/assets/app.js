2eb3cbbc080bff974846b860e8a22547da10a8b3
Danilo Di Moia <zitter@gmail.com>
2022-09-14 21:30:10 +0200

    Dropdown menu

--- app/assets/app.js
+++ app/assets/app.js
@@ -8,5 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import '@tabler/core/dist/js/tabler.min.js';
+
 // start the Stimulus application
 import './bootstrap';

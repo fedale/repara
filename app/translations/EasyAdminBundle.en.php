1e74c8be0d532cd9fc268e983c2960310a36d175
Danilo Di Moia <ddimoia>
2022-09-16 04:57:30 -0400

    Login restyle and Pagination start/end entries

---
+++ app/translations/EasyAdminBundle.en.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paginator' => [
+        'first' => 'First',
+        'previous' => 'Previous',
+        'next' => 'Next',
+        'last' => 'Last',
+        'counter' => 'Showing %start% to %end% of %results% entries',
+        'results' => '{0} No results|{1} <strong>1</strong> result|]1,Inf] <strong>%count%</strong> results',
+    ]
+];
---
+++ app/translations/EasyAdminBundle.it.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paginator' => [
+        'first' => 'Prima',
+        'previous' => 'Precedente',
+        'next' => 'Successiva',
+        'last' => 'Ultima',
+        'counter' => 'Visualizzati %start% a %end% di %results% risultati',
+        'results' => '{0} Nessun risultato|{1} <strong>1</strong> risultato|]1,Inf] <strong>%count%</strong> risultati',
+    ]
+];

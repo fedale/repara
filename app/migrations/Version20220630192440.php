aaf2cd4146b72e4669bd3b0000d0bfec38044c13
Danilo Di Moia <ddimoia>
2022-09-22 03:12:35 -0400

    Migrations customer to PostgreSQL

--- app/migrations/Version20220630192440.php
+++ app/migrations/Version20220630192440.php
@@ -20,14 +20,15 @@ final class Version20220630192440 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE customer_type (
-            id SMALLSERIAL PRIMARY KEY NOT NULL,
+            id SMALLSERIAL NOT NULL,
             name VARCHAR(255) DEFAULT NULL,
-            INDEX name (name),
             PRIMARY KEY(id)
-        ) ');
+        )');
+        $this->addSql('CREATE INDEX ON customer_type (name)');
+        
 
         $this->addSql('CREATE TABLE customer (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             code VARCHAR(64) NOT NULL,
             username VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL,
@@ -42,17 +43,18 @@ final class Version20220630192440 extends AbstractMigration
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            UNIQUE INDEX email (email),
-            UNIQUE INDEX username (username),
-            UNIQUE INDEX code (code),
-            INDEX type_id (type_id),
-            INDEX active (active),
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `customer_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE UNIQUE INDEX ON customer (email)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer (username)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer (code)');
+        $this->addSql('CREATE INDEX ON customer (type_id)');
+        $this->addSql('CREATE INDEX ON customer (active)');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT customer_ibfk_1 FOREIGN KEY (type_id) REFERENCES customer_type (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
+        
         
         $this->addSql('CREATE TABLE customer_attachment (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             customer_id INT NOT NULL,
             name VARCHAR(255) NOT NULL,
             type VARCHAR(32) NOT NULL,
@@ -63,46 +65,44 @@ final class Version20220630192440 extends AbstractMigration
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX created_at (created_at),
-            INDEX active (active),
-            INDEX size (size),
-            INDEX type_2 (type),
-            INDEX name (name),
-            INDEX filename (filename),
-            INDEX path (path),
-            INDEX type (type),
-            INDEX stuff_id (customer_id),
-            INDEX updated_at (updated_at),
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_attachment_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');        
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE INDEX ON customer_attachment  (active)');
+        $this->addSql('CREATE INDEX ON customer_attachment (size)');
+        $this->addSql('CREATE INDEX ON customer_attachment (type)');
+        $this->addSql('CREATE INDEX ON customer_attachment (name)');
+        $this->addSql('CREATE INDEX ON customer_attachment (filename)');
+        $this->addSql('CREATE INDEX ON customer_attachment (path)');
+        $this->addSql('CREATE INDEX ON customer_attachment (type)');
+        $this->addSql('CREATE INDEX ON customer_attachment (customer_id)');
+        $this->addSql('ALTER TABLE customer_attachment ADD CONSTRAINT customer_attachment_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
         
         $this->addSql('CREATE TABLE customer_location (
-            id SERIAL PRIMARY KEY NOT NULL,
-            customer_id INT NOT NULL,
+            id SERIAL NOT NULL,
+            customer_id INT NOT NULL CHECK (customer_id > 0),
             name VARCHAR(128) NOT NULL,
             address VARCHAR(64) NOT NULL,
             zipcode VARCHAR(8) DEFAULT NULL,
             city VARCHAR(64) NOT NULL,
-            country VARCHAR(32) DEFAULT \'Italia\' NOT NULL,
+            country VARCHAR(32) NOT NULL DEFAULT \'Italia\',
             active SMALLINT DEFAULT 1 NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX city (city),
-            INDEX active (active),
-            INDEX name (address),
-            INDEX zipcode (zipcode),
-            INDEX updated_at (updated_at),
-            INDEX created_at (created_at),
-            INDEX customer_id (customer_id),
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_location_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE INDEX ON customer_location (city)');
+        $this->addSql('CREATE INDEX ON customer_location (active)');
+        $this->addSql('CREATE INDEX ON customer_location (address)');
+        $this->addSql('CREATE INDEX ON customer_location (zipcode)');
+        $this->addSql('CREATE INDEX ON customer_location (updated_at)');
+        $this->addSql('CREATE INDEX ON customer_location (created_at)');
+        $this->addSql('CREATE INDEX ON customer_location (customer_id)');
+        $this->addSql('ALTER TABLE customer_location ADD CONSTRAINT customer_location_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
 
         $this->addSql('CREATE TABLE customer_contact (
-            id SERIAL PRIMARY KEY NOT NULL,
-            customer_location_id INT DEFAULT NULL,
+            id SERIAL NOT NULL,
+            customer_location_id INT DEFAULT NULL CHECK (customer_location_id > 0),
             firstname VARCHAR(64) NOT NULL,
             lastname VARCHAR(64) NOT NULL,
             phone VARCHAR(32) DEFAULT NULL,
@@ -111,60 +111,59 @@ final class Version20220630192440 extends AbstractMigration
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX active (active),
-            INDEX updated_at (updated_at),
-            INDEX firstname (firstname),
-            UNIQUE INDEX phone (phone, customer_location_id),
-            INDEX customer_location_id (customer_location_id),
-            INDEX created_at (created_at),
-            UNIQUE INDEX email (email, customer_location_id),
-            INDEX lastname (lastname),
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_contact_ibfk_1` FOREIGN KEY (`customer_location_id`) REFERENCES `customer_location` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
-
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE INDEX ON customer_contact (active)');
+        $this->addSql('CREATE INDEX ON customer_contact (updated_at)');
+        $this->addSql('CREATE INDEX ON customer_contact (firstname)');
+        $this->addSql('CREATE INDEX ON customer_contact (customer_location_id)');
+        $this->addSql('CREATE INDEX ON customer_contact (created_at)');
+        $this->addSql('CREATE INDEX ON customer_contact (lastname)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer_contact (phone, customer_location_id)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer_contact (email, customer_location_id)');
+        $this->addSql('ALTER TABLE customer_contact ADD CONSTRAINT customer_contact_ibfk_1 FOREIGN KEY (customer_location_id) REFERENCES customer_location (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
 
         $this->addSql('CREATE TABLE customer_location_place (
-            id SERIAL PRIMARY KEY NOT NULL,
-            customer_location_id INT NOT NULL,
+            id SERIAL NOT NULL,
+            customer_location_id INT NOT NULL CHECK (customer_location_id > 0),
             name VARCHAR(64) NOT NULL,
             active SMALLINT DEFAULT 1 NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX created_at (created_at),
-            INDEX updated_at (updated_at),
-            INDEX name (name),
-            INDEX active (active),
-            INDEX customer_location_id (customer_location_id),
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_location_place_ibfk_1` FOREIGN KEY (`customer_location_id`) REFERENCES `customer_location` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE INDEX ON customer_location_place (created_at)');
+        $this->addSql('CREATE INDEX ON customer_location_place (updated_at)');
+        $this->addSql('CREATE INDEX ON customer_location_place (name)');
+        $this->addSql('CREATE INDEX ON customer_location_place (active)');
+        $this->addSql('CREATE INDEX ON customer_location_place (customer_location_id)');
+        $this->addSql('ALTER TABLE customer_location_place ADD CONSTRAINT customer_location_place_ibfk_1 FOREIGN KEY (customer_location_id) REFERENCES customer_location (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
         
         $this->addSql('CREATE TABLE customer_location_place_asset (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             name VARCHAR(64) NOT NULL,
             code VARCHAR(64) NOT NULL,
-            customer_location_place_id INT NOT NULL,
-            asset_id INT NOT NULL,
+            customer_location_place_id INT NOT NULL CHECK (customer_location_place_id > 0),
+            asset_id INT NOT NULL CHECK (asset_id > 0),
             active SMALLINT DEFAULT 1 NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX updated_at (updated_at),
-            INDEX active (active),
-            INDEX name (name),
-            INDEX asset_id (asset_id),
-            INDEX created_at (created_at),
-            UNIQUE INDEX code (code),
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_location_place_asset_ibfk_1` FOREIGN KEY (`customer_location_place_id`) REFERENCES `customer_location_place` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-            CONSTRAINT `customer_location_place_asset_ibfk_2` FOREIGN KEY (`asset_id`) REFERENCES `asset` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset (updated_at)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset (active)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset (name)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset (asset_id)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset (created_at)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer_location_place_asset (code)');
+        $this->addSql('ALTER TABLE customer_location_place_asset ADD CONSTRAINT customer_location_place_asset_ibfk_1 FOREIGN KEY (customer_location_place_id) REFERENCES customer_location_place (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
+        $this->addSql('ALTER TABLE customer_location_place_asset ADD CONSTRAINT customer_location_place_asset_ibfk_2 FOREIGN KEY (asset_id) REFERENCES asset (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
         
         $this->addSql('CREATE TABLE customer_location_place_asset_attachment (
-            id SERIAL PRIMARY KEY NOT NULL,
-            customer_location_place_asset_id INT DEFAULT NULL,
+            id SERIAL NOT NULL,
+            customer_location_place_asset_id INT DEFAULT NULL CHECK (customer_location_place_asset_id > 0),
             name VARCHAR(255) NOT NULL,
             type VARCHAR(32) NOT NULL,
             size INT NOT NULL,
@@ -174,22 +173,22 @@ final class Version20220630192440 extends AbstractMigration
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX created_at (created_at),
-            INDEX active (active),
-            INDEX size (size),
-            INDEX type_2 (type),
-            INDEX name (name),
-            INDEX filename (filename),
-            INDEX path (path),
-            INDEX type (type),
-            INDEX stuff_id (customer_location_place_asset_id),
-            INDEX updated_at (updated_at),
             PRIMARY KEY(id)
-        ) ');
+        )');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (created_at)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (active)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (size)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (type)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (name)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (filename)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (path)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (type)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (customer_location_place_asset_id)');
+        $this->addSql('CREATE INDEX ON customer_location_place_asset_attachment (updated_at)');
         
         $this->addSql('CREATE TABLE customer_profile (
-            id SERIAL PRIMARY KEY NOT NULL,
-            customer_id INT NOT NULL,
+            id SERIAL NOT NULL,
+            customer_id INT NOT NULL CHECK (customer_id > 0),
             firstname VARCHAR(255) DEFAULT NULL,
             lastname VARCHAR(64) NOT NULL,
             public_email VARCHAR(255) DEFAULT NULL,
@@ -199,48 +198,49 @@ final class Version20220630192440 extends AbstractMigration
             website VARCHAR(255) DEFAULT NULL,
             bio TEXT DEFAULT NULL,
             timezone VARCHAR(40) DEFAULT NULL,
-            setting LONGTEXT DEFAULT NULL COMMENT \'settings preferences\',
-            PRIMARY KEY(id),
-            UNIQUE INDEX(customer_id),
-            CONSTRAINT `customer_profile_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            setting TEXT DEFAULT NULL,
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('CREATE UNIQUE INDEX ON customer_profile (customer_id)');
+        $this->addSql('ALTER TABLE customer_profile ADD CONSTRAINT customer_profile_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
+
 
         $this->addSql('CREATE TABLE customer_group (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             name VARCHAR(255) DEFAULT NULL,
-            INDEX name (name),
             PRIMARY KEY(id)
-        ) ');
+        )');
+        $this->addSql('CREATE INDEX ON customer_group (name)');
 
         $this->addSql('CREATE TABLE customer_group_assigned (
-            id SERIAL PRIMARY KEY NOT NULL,
-            customer_id INT NOT NULL,
-            customer_group_id INT NOT NULL,
-            PRIMARY KEY(id),
-            CONSTRAINT `customer_customer_group_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-            CONSTRAINT `customer_customer_group_ibfk_2` FOREIGN KEY (`customer_group_id`) REFERENCES `customer_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            id SERIAL NOT NULL,
+            customer_id INT NOT NULL CHECK (customer_id > 0),
+            customer_group_id INT NOT NULL CHECK (customer_group_id > 0),
+            PRIMARY KEY(id)
+        )');
+        $this->addSql('ALTER TABLE customer_group_assigned ADD CONSTRAINT customer_customer_group_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
+        $this->addSql('ALTER TABLE customer_group_assigned ADD CONSTRAINT customer_customer_group_ibfk_2 FOREIGN KEY (customer_group_id) REFERENCES customer_group (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
 
         $this->addSql('CREATE TABLE customer_role (
-            id SMALLSERIAL PRIMARY KEY NOT NULL,
+            id SMALLSERIAL NOT NULL,
             name VARCHAR(64) NOT NULL,
             slug VARCHAR(64) NOT NULL,
             code VARCHAR(64) NOT NULL,
-            INDEX name (name),
-            UNIQUE INDEX slug (slug),
-            UNIQUE INDEX code (code),
             PRIMARY KEY(id)
-        ) ');
+        )');
+        $this->addSql('CREATE INDEX ON customer_role (name)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer_role (slug)');
+        $this->addSql('CREATE UNIQUE INDEX ON customer_role (code)');
 
         $this->addSql('CREATE TABLE customer_role_assigned (
-            customer_id INT NOT NULL,
-            customer_role_id SMALLINT NOT NULL,
-            INDEX FK_D95AB405A76ED398 (customer_role_id),
-            INDEX IDX_CFC787FBA76ED396 (customer_id),
-            PRIMARY KEY(customer_id, customer_role_id),
-            CONSTRAINT `customer_customer_role_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-            CONSTRAINT `customer_customer_role_ibfk_2` FOREIGN KEY (`customer_role_id`) REFERENCES `customer_role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            customer_id INT NOT NULL CHECK (customer_id > 0),
+            customer_role_id SMALLINT NOT NULL CHECK (customer_role_id > 0),
+            PRIMARY KEY(customer_id, customer_role_id)
+        )');
+        $this->addSql('CREATE INDEX ON customer_role_assigned (customer_role_id)');
+        $this->addSql('CREATE INDEX ON customer_role_assigned (customer_id)');
+        $this->addSql('ALTER TABLE customer_role_assigned ADD CONSTRAINT customer_customer_role_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
+        $this->addSql('ALTER TABLE customer_role_assigned ADD CONSTRAINT customer_customer_role_ibfk_2 FOREIGN KEY (customer_role_id) REFERENCES customer_role (id) ON DELETE NO ACTION ON UPDATE NO ACTION');
     }
 
     public function down(Schema $schema): void

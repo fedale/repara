6baa8709e43b77833dbeec8d65c96f018f473844
Danilo Di Moia <ddimoia>
2022-09-22 06:27:46 -0400

    Migration project to PostgreSQL

--- app/migrations/Version20220630192480.php
+++ app/migrations/Version20220630192480.php
@@ -20,79 +20,83 @@ final class Version20220630192480 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE project_type (
-            id SMALLSERIAL PRIMARY KEY NOT NULL,
+            id SMALLSERIAL NOT NULL,
             name VARCHAR(128) NOT NULL,
             active SMALLINT DEFAULT 1 NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX active (active),
-            INDEX created_at (created_at),
-            INDEX updated_at (updated_at),
-            INDEX name (name),
             PRIMARY KEY(id)
-        ) ');
+        )');
+        $this->addSql('CREATE INDEX ON project_type (active)');
+        $this->addSql('CREATE INDEX ON project_type (created_at)');
+        $this->addSql('CREATE INDEX ON project_type (updated_at)');
+        $this->addSql('CREATE INDEX ON project_type (name)');
 
+        $this->addSql('CREATE TYPE state AS ENUM (\'requested\', \'rejected\', \'approved\', \'current\', \'dead\', \'completed\', \'on_hold\', \'signed\')');
         $this->addSql('CREATE TABLE project (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             code VARCHAR(32) NOT NULL,
             name VARCHAR(128) NOT NULL,
             description TEXT DEFAULT NULL,
             datetime_start TIMESTAMP DEFAULT NULL,
             datetime_end TIMESTAMP DEFAULT NULL,
-            state enum("requested", "rejected", "approved", "current", "dead", "completed", "on_hold", "signed") NOT NULL COMMENT \'DC2Type::ProjectTaskStateType\', 
+            state state NOT NULL, 
             budget NUMERIC(15, 2) DEFAULT NULL,
             color CHAR(6) DEFAULT NULL,
-            priority TINYINT(1) NOT NULL,
-            type_id SMALLINT NOT NULL,
-            visible TINYINT(1) DEFAULT 1 NOT NULL,
+            priority SMALLINT NOT NULL,
+            type_id SMALLINT NOT NULL CHECK (type_id > 0),
+            visible SMALLINT DEFAULT 1 NOT NULL,
             active SMALLINT DEFAULT 1 NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
             finished_at TIMESTAMP DEFAULT NULL,
-            INDEX name (name),
-            INDEX updated_at (updated_at),
-            INDEX color (color),
-            INDEX datetime_start (datetime_start),
-            INDEX active (active),
-            INDEX priority (priority),
-            INDEX datetime_end (datetime_end),
-            INDEX visible (visible),
-            INDEX state (state),
-            UNIQUE INDEX code (code),
-            INDEX created_at (created_at),
-            INDEX budget (budget),
             PRIMARY KEY(id),
-            CONSTRAINT `project_type_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `project_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            CONSTRAINT project_type_ibfk_1 FOREIGN KEY (type_id) REFERENCES project_type (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+        )');
+        $this->addSql('COMMENT ON COLUMN project.state IS \'DC2Type::ProjectTaskStateType\'');
+        $this->addSql('CREATE INDEX ON project (name)');
+        $this->addSql('CREATE INDEX ON project (updated_at)');
+        $this->addSql('CREATE INDEX ON project (color)');
+        $this->addSql('CREATE INDEX ON project (datetime_start)');
+        $this->addSql('CREATE INDEX ON project (active)');
+        $this->addSql('CREATE INDEX ON project (priority)');
+        $this->addSql('CREATE INDEX ON project (datetime_end)');
+        $this->addSql('CREATE INDEX ON project (visible)');
+        $this->addSql('CREATE INDEX ON project (state)');
+        $this->addSql('CREATE INDEX ON project (created_at)');
+        $this->addSql('CREATE INDEX ON project (budget)');
+        $this->addSql('CREATE UNIQUE INDEX ON project (code)');
         
         $this->addSql('CREATE TABLE project_activity (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             name VARCHAR(128) NOT NULL,
-            TIMESTAMP DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            user_id INT NOT NULL,
-            project_id INT NOT NULL,
-            INDEX name (name),
-            INDEX user_id (user_id),
-            INDEX project_id (project_id),
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            user_id INT NOT NULL CHECK (user_id > 0),
+            project_id INT NOT NULL CHECK (project_id > 0),
             PRIMARY KEY(id),
-            CONSTRAINT `project_activity_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-            CONSTRAINT `project_activity_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-        ) ');
+            CONSTRAINT project_activity_ibfk_1 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+            CONSTRAINT project_activity_ibfk_2 FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+        )');
+        $this->addSql('CREATE INDEX ON project_activity (name)');
+        $this->addSql('CREATE INDEX ON project_activity (user_id)');
+        $this->addSql('CREATE INDEX ON project_activity (project_id)');
+        // $this->addSql('ALTER TABLE ');
+        // $this->addSql('ALTER TABLE ');
 
         $this->addSql('CREATE TABLE project_milestone (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL NOT NULL,
             name VARCHAR(32) NOT NULL,
             expiration_date TIMESTAMP NOT NULL,
             active SMALLINT DEFAULT 1 NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at TIMESTAMP DEFAULT NULL,
-            INDEX active (active),
-            INDEX name (name),
             PRIMARY KEY(id)
-        ) ');
+        )');
+        $this->addSql('CREATE INDEX ON project_milestone (active)');
+        $this->addSql('CREATE INDEX ON project_milestone (name)');
     }
 
     public function down(Schema $schema): void

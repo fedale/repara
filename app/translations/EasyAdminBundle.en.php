0dfffb299dcd088918db3f1182b445dbf7841c7e
Danilo Di Moia <ddimoia>
2022-09-30 11:48:51 -0400

    New layout using EA CSS

--- app/translations/EasyAdminBundle.en.php
+++ app/translations/EasyAdminBundle.en.php
@@ -7,6 +7,15 @@ return [
         'next' => 'Next',
         'last' => 'Last',
         'counter' => 'Showing %start% to %end% of %results% entries',
-        'results' => '{0} No results|{1} <strong>1</strong> result|]1,Inf] <strong>%count%</strong> results',
-    ]
+        'results' => '{0} No results|{1} <strong>1</strong> result|]1,Inf] <strong>%count%</strong> results'
+    ],
+    'settings' => [
+        'appearance' => [
+            'label' => 'label',
+            'light' => 'light',
+            'dark' => 'dark',
+            'auto' => 'auto',
+            'choose' => 'Choose:'
+        ],
+    ],
 ];

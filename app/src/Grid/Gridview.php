90a99dd3c099d973716b4b523b225f4e5794b262
Danilo Di Moia <d.dimoia@waltertosto.it>
2023-02-17 17:28:11 +0100

    -

--- app/src/Grid/Gridview.php
+++ app/src/Grid/Gridview.php
@@ -3,6 +3,7 @@ namespace App\Grid;
 
 use App\Entity\Customer\Customer;
 use App\Grid\Column\ColumnInterface;
+use App\Grid\DataProvider\DataProviderInterface;
 use App\Grid\Source\SourceInterface;
 use APY\DataGridBundle\Grid\Columns;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,7 +14,33 @@ class Gridview {
 
     private $twig;
     private $columns;
-    private $dataProvider;
+    private DataProviderInterface $dataProvider;
+    /**
+     * @var string the HTML display when the content of a cell is empty.
+     * This property is used to render cells that have no defined content,
+     * e.g. empty footer or filter cells.
+     *
+     * Note that this is not used by the [[DataColumn]] if a data item is `null`. In that case
+     * the [[\yii\i18n\Formatter::nullDisplay|nullDisplay]] property of the [[formatter]] will
+     * be used to indicate an empty data value.
+     */
+    public $emptyCell = '&nbsp;';
+    /**
+     * Gridview options
+     */
+    private $options = [];
+    /**
+     * @var \App\Service\GridFilter|null the model that keeps the user-entered filter data. When this property is set,
+     * the grid view will enable column-based filtering. Each data column by default will display a text field
+     * at the top that users can fill in to filter the data.
+     *
+     * Note that in order to show an input field for filtering, a column must have its [[DataColumn::attribute]]
+     * property set and the attribute should be active in the current scenario of $filterModel or have
+     * [[DataColumn::filter]] set as the HTML code for the input field.
+     *
+     * When this property is not set (null) the filtering feature is disabled.
+     */
+    public \App\Service\GridFilter|null $filterService;
 
     public function __construct(Environment $twig)
     {
